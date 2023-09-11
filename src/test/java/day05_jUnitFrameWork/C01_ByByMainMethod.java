5d105b0831240c122509d1dfa54bc69118c1c2a6
OMEROZYIGIT <ozyigitomer9@gmail.com>
2023-09-12 00:26:42 +0300

    first commit

---
+++ src/test/java/day05_jUnitFrameWork/C01_ByByMainMethod.java
@@ -0,0 +1,37 @@
+package day05_jUnitFrameWork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class C01_ByByMainMethod {
+
+     @Before
+    public void setUp(){
+         System.out.println("Setup method is executed");
+    }
+    @After
+    public void tearDown(){
+        System.out.println("Tear method is executed");
+    }
+
+    @Test
+    public void test01(){
+        // Test to the @Test annotation a method can be executed
+        // if there ise no assert
+
+        System.out.println("Test01 is executed");
+    }
+    @Test
+    public void test02(){
+
+        System.out.println("Test02 is executed");
+    }
+
+    @Test
+    public void test03(){
+
+        System.out.println("Test03 is executed");
+    }
+
+}
